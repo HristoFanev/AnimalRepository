9bd2140ecd8598698cc36f9ac65b75e2d5de6e6e
HristoFanev <hristo2204@abv.bg>
2023-01-16 20:51:28 +0200

    Adding MVC and connecting it to the database

---
+++ AnimalRepositoryExercise/ProjectMVC/Controllers/AnimalController.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Implementation;
+using DataAccess.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectMVC.Controllers
+{
+    public class AnimalController : Controller
+    {
+        Animal animal = new Animal();
+        AnimalRepositoryImplementation<Animal> a = new AnimalRepositoryImplementation<Animal>();
+        public IActionResult Index()
+        {
+            return View(a.GetAll());
+        }
+    }
+}
---
+++ AnimalRepositoryExercise/ProjectMVC/Controllers/BreedController.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Implementation;
+using DataAccess.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectMVC.Controllers
+{
+    public class BreedController : Controller
+    {
+        Breed breed = new Breed();
+        AnimalRepositoryImplementation<Breed> b = new AnimalRepositoryImplementation<Breed>();
+        public IActionResult Index()
+        {
+            return View(b.GetAll());
+        }
+    }
+}
